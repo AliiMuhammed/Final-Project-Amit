c7c3c02fffdb04291d14b28a64930de1d269d900
AliiMuhammed <mohamedtaha8000@gmail.com>
2024-07-30 05:14:58 +0300

    finish navbar with resposive

--- bistro/src/App.js
+++ bistro/src/App.js
@@ -2,11 +2,13 @@ import React, { Suspense } from "react";
 import { Outlet } from "react-router";
 import MoveToTop from "./Shared/MoveToTop";
 import Backdrop from "./Shared/Backdrop";
+import NavBar from "./Shared/NavBar";
 
 function App() {
   return (
     <>
       <Suspense fallback={<Backdrop />}>
+        <NavBar/>
         <Outlet />
       </Suspense>
       <MoveToTop />
---
+++ bistro/src/Shared/NavBar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { FiPhone, FiMail } from "react-icons/fi";
+import { FaTwitter, FaFacebookF, FaInstagram, FaGithub } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../Assets/logo.png";
+import "../Style/navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
+
+const NavBar = () => {
+  const [show, setShowe] = useState(false);
+  return (
+    <header className="main-header">
+      <div className="top-header">
+        <div className="container">
+          <div className="contact-info">
+            <div>
+              <FiPhone />
+              (414) 857 - 0107
+            </div>
+            <div>
+              <FiMail />
+              yummy@bistrobliss
+            </div>
+          </div>
+          <div className="social-icons">
+            <Link to={"/"}>
+              <FaTwitter />
+            </Link>
+            <Link to={"/"}>
+              <FaFacebookF />
+            </Link>
+            <Link to={"/"}>
+              <FaInstagram />
+            </Link>
+            <Link to={"/"}>
+              <FaGithub />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <nav>
+        <div className="container">
+          <div className="site-logo">
+            <img src={logo} alt="site-logo" />
+          </div>
+          <div className={`navgations ${show ? "active" : ""}`}>
+            <ul>
+              <li>
+                <NavLink to={"/"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/about"}>About</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/menu"}>Menu</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/pages"}>Pages</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/contact"}>Contact</NavLink>
+              </li>
+              <li>
+                <button className="main-outline-btn">Book A Table</button>
+              </li>
+            </ul>
+          </div>
+          <button
+            className={`dropdown-btn ${show ? "active" : ""}`}
+            onClick={() => setShowe(!show)}
+          >
+            {show ? <IoCloseOutline /> : <RxHamburgerMenu />}
+          </button>
+          <button className="main-outline-btn">Book A Table</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
